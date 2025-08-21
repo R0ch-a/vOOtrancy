c8ccef3f6ada80420d1270b870d65679edef42d5
roch-afel <rafael.rocha@dcomp.ufs.br>
2025-08-20 22:39:18 -0300

    Definição das classes Aeroporto e Avião
    A classe avião foi modificada para não herdar de uma classe abstrata.

--- src/main/java/com/vootrancy/model/entities/Aeroporto.java
+++ src/main/java/com/vootrancy/model/entities/Aeroporto.java
@@ -1,8 +1,33 @@
 package com.vootrancy.model.entities;
 
 public class Aeroporto {
-    // cidade
-    // nome
-    // sigla do aeroporto
-    // tempo de voo
+    private String nome;
+    private String sigla;
+    private String cidade;
+    private double distancia;
+    private double tempoVoo;
+    
+    public Aeroporto (String nome, String sigla, String cidade, double distancia, double tempo){
+        this.nome = nome;
+        this.sigla = sigla;
+        this.cidade = cidade;
+        this.distancia = distancia;
+        this.tempoVoo = tempo;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    public String getSigla(){
+        return sigla;
+    }
+    public String getCidade(){
+        return cidade;
+    }
+    public double getDistancia(){
+        return distancia;
+    }
+    public double getTempoVoo(){
+        return tempoVoo;
+    }
 }
