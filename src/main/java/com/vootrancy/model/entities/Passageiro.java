f79e90cd54d83bddf5c15753e00fe9a9a1d1cca7
roch-afel <rafael.rocha@dcomp.ufs.br>
2025-08-24 01:30:57 -0300

    Correção da classe Passageiro

--- src/main/java/com/vootrancy/model/entities/Passageiro.java
+++ src/main/java/com/vootrancy/model/entities/Passageiro.java
@@ -5,15 +5,13 @@ public class Passageiro {
     private int documentoID;
     private String nome;
     private int qtdeBagagens;
-    private Voo voo;
     private Date nascimento;
     private String genero;
 
-    public Passageiro (int id, String nome, int qtde, Voo voo, Date nascimento, String genero){
+    public Passageiro (int id, String nome, int qtde, Date nascimento, String genero){
         this.documentoID = id;
         this.nome = nome;
         this.qtdeBagagens = qtde;
-        this.voo = voo;
         this.nascimento = nascimento;
         this.genero = genero;
     }
@@ -30,10 +28,6 @@ public class Passageiro {
         return this.qtdeBagagens;
     }
   
-    public Voo getVoo(){
-        return voo;
-    }
-  
     public Date getNascimento(){
         return nascimento;
       
@@ -47,9 +41,6 @@ public class Passageiro {
     // Tudo que precisar adicionar ao clicar botoes chamara metodos.
     public void setQtdeBagagens(int qtde){
         qtdeBagagens = qtde;
-    }
-    public void setVoo(Voo voo){
-        this.voo = voo;
     }
 
     // Metodo toString
